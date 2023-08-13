aabb3fc51f90b031326349947b40ed382c86f180
Hiroki Paico <hiroki.paico@hotmail.com>
2023-08-12 23:48:54 -0500

    feat: Update entity to project instructions

--- apps/planets/src/entities/planet.entity.ts
+++ apps/planets/src/entities/planet.entity.ts
@@ -1,49 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryColumn, Column } from 'typeorm';
 
 @Entity()
 export class Planet {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn()
   id: number;
 
-  @Column({ length: 100, nullable: false })
+  @Column({ type: 'varchar', length: 100, nullable: false })
   name: string;
 
-  @Column({ type: 'int', nullable: true })
-  rotation_period: number;
-
-  @Column({ type: 'int', nullable: true })
-  orbital_period: number;
-
-  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
-  diameter: number;
-
-  @Column({ length: 100, nullable: true })
+  @Column({ type: 'varchar', length: 100, nullable: true })
   climate: string;
 
-  @Column({ length: 100, nullable: true })
+  @Column({ type: 'varchar', length: 10, nullable: true })
+  diameter: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
   gravity: string;
 
-  @Column({ length: 100, nullable: true })
-  terrain: string;
-
-  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
-  surface_water: number;
-
-  @Column({ length: 20, nullable: true })
+  @Column({ type: 'varchar', length: 20, nullable: true })
   population: string;
 
   @Column({ type: 'simple-array', nullable: true })
   residents: string[];
 
-  @Column({ type: 'simple-array', nullable: true })
-  films: string[];
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  terrain: string;
 
-  @Column({ type: 'timestamp', nullable: true })
-  created: Date;
-
-  @Column({ type: 'timestamp', nullable: true })
-  edited: Date;
-
-  @Column({ length: 200, nullable: true })
+  @Column({ type: 'varchar', length: 200, nullable: true })
   url: string;
 }
