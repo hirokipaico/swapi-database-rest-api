2dbe00d255b061c1ca098d86cce2d6e2a2f90740
Hiroki Paico <hiroki.paico@hotmail.com>
2023-08-13 06:35:13 -0500

    chore: Add controllers unit tests

--- apps/people/src/controllers/people.controller.spec.ts
+++ apps/people/src/controllers/people.controller.spec.ts
@@ -1,22 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PeopleController } from './people.controller';
 import { PeopleService } from '../services/people.service';
+import { Person } from '../entities/person.entity';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('PeopleController', () => {
   let peopleController: PeopleController;
+  let peopleService: PeopleService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [PeopleController],
-      providers: [PeopleService],
+      providers: [
+        {
+          provide: PeopleService,
+          useValue: {
+            getAllPeopleFromSWAPI: jest.fn(),
+            getPersonById: jest.fn(),
+            getNextAvailableId: jest.fn(),
+            savePerson: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    peopleController = app.get<PeopleController>(PeopleController);
+    peopleController = module.get<PeopleController>(PeopleController);
+    peopleService = module.get<PeopleService>(PeopleService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(peopleController.getHello()).toBe('Hello World!');
+  describe('getAllPeople', () => {
+    it('should return an array of people', async () => {
+      const mockPeople: Person[] = [];
+
+      jest
+        .spyOn(peopleService, 'getAllPeopleFromSWAPI')
+        .mockResolvedValue(mockPeople);
+
+      const result = await peopleController.getAllPeople({ page: 1 });
+      expect(result).toEqual(mockPeople);
+    });
+
+    it('should throw an InternalServerErrorException if service throws an error', async () => {
+      jest
+        .spyOn(peopleService, 'getAllPeopleFromSWAPI')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      await expect(peopleController.getAllPeople({ page: 1 })).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('getPersonById', () => {
+    it('should return a specific person with ID 1', async () => {
+      const mockPerson: Person = {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair',
+        eye_color: 'blue',
+        birth_year: '19BBY',
+        gender: 'male',
+        homeworld: 'https://swapi.dev/api/planets/1/',
+        created: '2014-12-09T13:50:51.644000Z',
+        edited: '2014-12-20T21:17:56.891000Z',
+        url: 'https://swapi.dev/api/people/1/',
+        id: 1,
+      };
+
+      jest.spyOn(peopleService, 'getPersonById').mockResolvedValue(mockPerson);
+
+      const result = await peopleController.getPersonById({ id: 1 });
+      expect(result).toEqual(mockPerson);
+    });
+
+    it('should throw an InternalServerErrorException if service throws an error', async () => {
+      jest
+        .spyOn(peopleService, 'getPersonById')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      await expect(peopleController.getPersonById({ id: 1 })).rejects.toThrow(
+        InternalServerErrorException,
+      );
     });
   });
 });
--- apps/planets/src/controllers/planet.controller.spec.ts
+++ apps/planets/src/controllers/planet.controller.spec.ts
@@ -1,18 +1,136 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlanetController } from './planet.controller';
+import { PlanetService } from '../services/planet.service';
+import { Planet } from '../entities/planet.entity';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 describe('PlanetController', () => {
-  let controller: PlanetController;
+  let planetController: PlanetController;
+  let planetService: PlanetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PlanetController],
+      providers: [
+        {
+          provide: PlanetService,
+          useValue: {
+            getPlanetsUntilPageFromSWAPI: jest.fn(),
+            getPlanetById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<PlanetController>(PlanetController);
+    planetController = module.get<PlanetController>(PlanetController);
+    planetService = module.get<PlanetService>(PlanetService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAllPlanets', () => {
+    it('Should return an array of planets', async () => {
+      const mockPlanets: Planet[] = [
+        {
+          id: null,
+          name: 'Tatooine',
+          climate: 'arid',
+          diameter: '10465',
+          gravity: '1 standard',
+          population: '200000',
+          residents: [
+            'https://swapi.dev/api/people/1/',
+            'https://swapi.dev/api/people/2/',
+            'https://swapi.dev/api/people/4/',
+            'https://swapi.dev/api/people/6/',
+            'https://swapi.dev/api/people/7/',
+            'https://swapi.dev/api/people/8/',
+            'https://swapi.dev/api/people/9/',
+            'https://swapi.dev/api/people/11/',
+            'https://swapi.dev/api/people/43/',
+            'https://swapi.dev/api/people/62/',
+          ],
+          terrain: 'desert',
+          url: 'https://swapi.dev/api/planets/1/',
+        },
+        {
+          id: null,
+          name: 'Yavin IV',
+          climate: 'temperate, tropical',
+          diameter: '10200',
+          gravity: '1 standard',
+          population: '1000',
+          residents: [],
+          terrain: 'jungle, rainforests',
+          url: 'https://swapi.dev/api/planets/3/',
+        },
+      ];
+
+      jest
+        .spyOn(planetService, 'getPlanetsUntilPageFromSWAPI')
+        .mockResolvedValue(mockPlanets);
+
+      const result = await planetController.getAllPlanets({ page: 1 });
+
+      const expectedNames = mockPlanets.map((mockPlanet) => mockPlanet.name);
+      const actualNames = result.map((resultPlanet) => resultPlanet.name);
+
+      // Check if each expected name is present in the actual names
+      expectedNames.forEach((expectedName) => {
+        expect(actualNames).toContain(expectedName);
+      });
+    });
+
+    it('should throw an InternalServerErrorException if service throws an error', async () => {
+      jest
+        .spyOn(planetService, 'getPlanetsUntilPageFromSWAPI')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      await expect(planetController.getAllPlanets({ page: 1 })).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
+  });
+
+  describe('getPlanetById', () => {
+    it('should return specific planet based on ID 2', async () => {
+      const mockPlanet: Planet = {
+        name: 'Alderaan',
+        diameter: '12500',
+        climate: 'temperate',
+        gravity: '1 standard',
+        terrain: 'grasslands, mountains',
+        population: '2000000000',
+        residents: [
+          'https://swapi.dev/api/people/5/',
+          'https://swapi.dev/api/people/68/',
+          'https://swapi.dev/api/people/81/',
+        ],
+        url: 'https://swapi.dev/api/planets/2/',
+        id: 2,
+      };
+      jest.spyOn(planetService, 'getPlanetById').mockResolvedValue(mockPlanet);
+      const result = await planetController.getPlanetById({ id: 2 });
+      expect(result).toBe(mockPlanet);
+    });
+
+    it(`should throw a BadRequestException due to negative query string value`, async () => {
+      try {
+        await planetController.getAllPlanets({ page: -2 });
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+      }
+    });
+
+    it(`shoud throw an InternalServerErrorException if service throws an error`, async () => {
+      jest
+        .spyOn(planetService, 'getPlanetById')
+        .mockRejectedValue(new InternalServerErrorException());
+
+      await expect(planetController.getPlanetById({ id: 10 })).rejects.toThrow(
+        InternalServerErrorException,
+      );
+    });
   });
 });
