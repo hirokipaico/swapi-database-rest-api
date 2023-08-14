6079b9a58fd459782097e34dc4456632c5dd018e
Hiroki Paico <hiroki.paico@hotmail.com>
2023-08-14 12:16:53 -0500

    fix: Update validation pipe for non-empty positive integer

--- libs/core/src/pipes/positive-integer.pipe.ts
+++ libs/core/src/pipes/positive-integer.pipe.ts
@@ -8,27 +8,13 @@ import {
 @Injectable()
 export class PositiveIntPipe implements PipeTransform<number, number> {
   transform(value: number, metadata: ArgumentMetadata): number {
-    if (typeof value !== 'number') {
+    if (!value || isNaN(+value) || +value < 1 || !Number.isInteger(+value)) {
       throw new BadRequestException(
         `${
           metadata.type.charAt(0).toUpperCase() + metadata.type.slice(1)
-        } element '${metadata.data}' must be an integer.`,
+        } element '${metadata.data}' must be a non-empty positive integer.`,
       );
     }
-    if (!value) {
-      throw new BadRequestException(
-        `${
-          metadata.type.charAt(0).toUpperCase() + metadata.type.slice(1)
-        } element '${metadata.data}' must not be empty.`,
-      );
-    }
-    if (value < 1) {
-      throw new BadRequestException(
-        `${
-          metadata.type.charAt(0).toUpperCase() + metadata.type.slice(1)
-        } element '${metadata.data}' must be higher than 0.`,
-      );
-    }
-    return value;
+    return +value;
   }
 }
